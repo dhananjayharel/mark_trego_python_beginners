07e8298c0717790d9e9ead196fc3d3d48d5f7fb9
dhananjay harel <harel.dhananjay@gmail.com>
2019-12-12 22:40:15 +0530

    employee main method

---
+++ employee_main_method/employee.py
@@ -0,0 +1,18 @@
+class Employee:
+    'Base class for Employee'
+    #Class Variables
+    employeeCount = 0
+    payRate = 10
+
+	
+def __init__(self):
+	self.firstName = 'N/A'
+	self.lastName = 'N/A'
+	self.uid = 'N/A'
+
+def main():
+	print('This Python program is now starting...')
+	print('The instructions will now be executed...')
+
+	
+main()
